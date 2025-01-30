344e6d3e18de3c68a1951e1f59ba990f7e07bfc9
sabrinayohana <yohana.s@escolar.ifrn.edu.br>
2025-01-29 21:44:23 -0300

    att background

--- fastCar.py
+++ fastCar.py
@@ -2,12 +2,16 @@ import pygame
 
 pygame.init()
 
+background = pygame.image.load('pista.jpg')
 tela = pygame.display.set_mode((600, 1000))
 pygame.display.set_caption('FastCar')
 
 tela_aberta = True
 while tela_aberta:
+    pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
           tela_aberta = False
+    tela.blit(background, (0,0))
+    pygame.display.update()
 pygame.quit()
